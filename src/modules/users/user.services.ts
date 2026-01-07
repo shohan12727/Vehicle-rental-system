a736a30e051461319e9073efc5dc04091c5c151a
shohan12727 <aishohan001@gmail.com>
2026-01-07 11:40:11 +0600

    hashed password added with bcrypt

--- src/modules/users/user.services.ts
+++ src/modules/users/user.services.ts
@@ -1,13 +1,20 @@
+import bcrypt from "bcryptjs";
 import { pool } from "../../database/db";
 
 const createdUserIntoDB = async (payLoad: Record<string, unknown>) => {
   const { name, email, password, phone, role } = payLoad;
+
+
+  const hashedPassword = await bcrypt.hash(password as string, 12)
+
   const result = await pool.query(
     `
         INSERT INTO users(name, email, password, phone, role) VALUES($1, $2, $3, $4, $5) RETURNING *
         `,
-    [name, email, password, phone, role]
+    [name, email, hashedPassword, phone, role]
   );
+
+//   delete result.rows[0].password
 
   return result;
 };
